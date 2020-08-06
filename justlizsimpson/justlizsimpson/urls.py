d7520b9fc3c5d2919022238fa8d81cca5141eccf
plasticglasses <33756800+plasticglasses@users.noreply.github.com>
2020-08-06 13:44:54 +0100

    added projects app with data

--- justlizsimpson/justlizsimpson/urls.py
+++ justlizsimpson/justlizsimpson/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#my project views
+from projects.views import project_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('projects/', project_view, name='projects')
 ]
--- justlizsimpson/projects/models.py
+++ justlizsimpson/projects/models.py
@@ -7,4 +7,4 @@ class Project(models.Model):
     description = models.TextField()
     date = models.DateTimeField(auto_now=False, auto_now_add=False)
     github = models.URLField(max_length=200)
-    thumbnail = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    thumbnail = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, blank=True)
